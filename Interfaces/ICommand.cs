59a3b9e0538e11be4588f12d26ec7347cabb9a56
Onoda Yoshiaki <y-onoda@nikki-grp.co.jp>
2015-12-11 15:14:32 +0900

    ICommand追加

---
+++ Interfaces/ICommand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Patterns.Interfaces
+{
+    public interface ICommand
+    {
+        bool CanExecute(Object parameter);
+        void Execute(Object parameter);
+
+        event EventHandler CanExecuteChanged;
+    }
+}
